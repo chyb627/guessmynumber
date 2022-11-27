b2999a395d74aa872e9a8185520e99481b2ac6dd
cha <chyb627@naver.com>
2022-11-27 22:31:46 +0900

    화면 스타일링

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
+  // https://prettier.io/docs/en/configuration.html 참고
+  bracketSpacing: true,
   bracketSameLine: true,
-  bracketSpacing: false,
+  semi: true,
+  printWidth: 120,
   singleQuote: true,
   trailingComma: 'all',
 };
---
+++ src/screens/StartGameScreen.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { TextInput, View, StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad" // 숫자 패드 사용
+        autoCapitalize="none" // 첫글자 대문자 취소
+        autoCorrect={false} // 자동수정 기능 off
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    elevation: 4, // 그림자, 안드로이드용, 0~8
+    shadowColor: 'black', // 그림자, ios용
+    shadowOffset: { width: 0, height: 2 }, // 그림자 가로, 세로 늘어남, 단위 픽셀
+    shadowRadius: 6, // 그림자가 얼마나 번지는지
+    shadowOpacity: 0.25, // 그림자 투명도
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default StartGameScreen;
