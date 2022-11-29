3e9db2d55dbacef74a496ba3337114ae89137979
cha <chyb627@naver.com>
2022-11-29 23:29:59 +0900

    그라데이션 추가하기

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import StartGameScreen from './src/screens/StartGameScreen';
 
 const App = () => {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 };
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
 
 export default App;
--- src/screens/StartGameScreen.tsx
+++ src/screens/StartGameScreen.tsx
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#4e0329',
+    backgroundColor: '#3b021f',
     borderRadius: 8,
     elevation: 4, // 그림자, 안드로이드용, 0~8
     shadowColor: 'black', // 그림자, ios용
