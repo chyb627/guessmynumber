76946af932bc02d44386b8e9cc40cd17d6a3d6e5
cha <chyb627@naver.com>
2022-12-01 00:10:30 +0900

    react-native-vector-icons

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 import NumberContainer from '../components/game/NumberContainer';
 import Card from '../components/ui/Card';
@@ -58,10 +59,14 @@ function GameScreen({ userNumber, onGameOver }) {
         <InstructionText style={styles.InstructionText}>Higher or lower?</InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Icon name="minus" size={24} color="white" />
+            </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+              <Icon name="plus" size={24} color="white" />
+            </PrimaryButton>
           </View>
         </View>
       </Card>
